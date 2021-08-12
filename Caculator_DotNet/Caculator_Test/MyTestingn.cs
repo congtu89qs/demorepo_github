3e7df96da6252e02932598ce87f96dc7890fb7b3
TECHVN\tutc <tutc@techvn.mti.co.jp>
2021-08-12 10:31:00 +0700

    commit source code

---
+++ Caculator_DotNet/Caculator_Test/MyTestingn.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using UnitTestingDemo;
+
+namespace Caculator_Test
+{
+    [TestFixture]
+    public class MyTestingn
+    {
+        [Test]
+        public void Add_Test()
+        {
+            Calculator cal = new Calculator();
+
+            int add_Result = cal.Add(3, 5);
+
+            Assert.That(add_Result, Is.EqualTo(8));
+        }
+
+
+        [Test]
+        public void Sub_Test()
+        {
+            Calculator cal = new Calculator();
+
+            int sub_Result = cal.Sub(3, 5);
+
+            Assert.That(sub_Result, Is.EqualTo(-2));
+        }
+    }
+}
---
+++ Caculator_DotNet/UnitTestingDemo/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTestingDemo
+{
+    public class Calculator
+
+    {
+
+        public int Add(int a, int b)
+        {
+
+            return a + b;
+
+        }
+
+        public int Sub(int a, int b)
+
+        {
+
+            return a - b;
+
+        }
+
+    }
+}
